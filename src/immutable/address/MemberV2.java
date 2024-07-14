d1dbadb4b7fbe8557bf3d75d984d4bdb273c93d0
hobe0m <beom231231@gmail.com>
2024-07-14 19:43:27 +0900

    [finish]Immutable

---
+++ src/immutable/address/MemberV2.java
@@ -0,0 +1,30 @@
+package immutable.address;
+
+public class MemberV2 {
+    // 불변 객체의 예제
+
+    // 불변 클래스의 사용
+    private String name;
+    private ImmutableAddress address;
+
+    public MemberV2(String name, ImmutableAddress address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public ImmutableAddress getAddress() {
+        return address;
+    }
+
+    public void setAddress(ImmutableAddress address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "MemberV1{" +
+                "name='" + name + '\'' +
+                ", address=" + address +
+                '}';
+    }
+}
