3356a42edd5442b4444b447d28b2391ec16d2250
hobe0m <beom231231@gmail.com>
2024-08-04 17:05:07 +0900

    [finish]Nested

--- src/nested/nested/NestedOuter.java
+++ src/nested/nested/NestedOuter.java
@@ -1,19 +1,43 @@
 package nested.nested;
 
 public class NestedOuter {
+    // 정적 중첩 클래스
+    // 정적 중첩 클래스는 앞에 static이 붙는다.
+    // 자신의 멤버에는 당연히 접근 가능
+    // 바깥의 클래스 멤버에 접근 가능
+    // 바깥의 인스턴스 멤버에는 접근 불가능
+
+    // static value 생성
     private static int outClassValue = 3;
+
+    // instance value 생성
     private int outInstanceValue = 2;
+
+    // 정적 중첩 클래스 생성
+    // 클래스 안에 클래스 생성 가능
     static class Nested {
+        // 멤버 변수 생성
         private int nestedInstanceValue = 1;
+
+        // 자신의 멤버에 접근하는 메서드 생성
         public void print() {
-
             // 자신의 멤버에 접근
-
             System.out.println(nestedInstanceValue);
+
             // 바깥 클래스의 인스턴스 멤버에는 접근할 수 없다.
+            // static이기에 static(클래스 영역)은 당연히 instance(인스턴스 영역)에 접근할 수 없다.
             // System.out.println(outInstanceValue);
+
             // 바깥 클래스의 클래스 멤버에는 접근할 수 있다(private도 접근 가능).
+            // 원래도 클래스 영역을 통해 static 즉, 클래스 멤버에는 접근할 수 있지만 정적 중첩 클래스는 private 멤버에도 접근할 수 있다는 점이 다르다.
             System.out.println(NestedOuter.outClassValue);
+
+            // 바깥 클래스 이름 + dot(.)을 사용하지 않아도 접근할 수 있다.
+            System.out.println(outClassValue);
+
+            // private 접근 제어자
+            // private 접근 제어자는 같은 클래스 안에 있을 때만 접근할 수 있다.
+            // 중첩 클래스도 바깥 클래스와 같은 클래스 안에 있으므로 중첩 클래스 또한 바깥 클래스의 private 접근 제어자에 접근할 수 있다.
         }
     }
 }
--- src/nested/nested/NestedOuterMain.java
+++ src/nested/nested/NestedOuterMain.java
@@ -2,10 +2,29 @@ package nested.nested;
 
 public class NestedOuterMain {
     public static void main(String[] args) {
+        // 외부 클래스 없이 내부에 중첩 클래스의 인스턴스만 생성해도 된다.
+        // 외부, 중첩 클래스의 인스턴스는 연관이 없기 때문이다.
         NestedOuter outer = new NestedOuter();
+
+        // 내부의 정적 중첩 클래스의 객체를 생성하고 싶을 때는 외부 클래스.내부 클래스의 형식을 사용하면 된다.
         NestedOuter.Nested nested = new NestedOuter.Nested();
         nested.print();
 
+        // 클래스 정보 확인
+        // 내부의 클래스는 외부 클래스 + $ + 내부 클래스의 형식으로 표현된다.
         System.out.println("nestedClass = " + nested.getClass());
+
+        // 정적 중첩 클래스는 new 바깥 클래스.중첩 클래스()로 생성할 수 있다.
+        // 따라서 NestedOuter.Nested와 같이 접근이 가능하다.
+        // 여기서 outer와 nested 인스턴스는 서로 아무 관계가 없는 인스턴스이다.
+        // 단지 클래스 구조 상 중첩해 두었을 뿐이다.
+        // 따라서 둘이 아무런 관련이 없으므로 정적 중첩 클래스의 인스턴스만 따로 생성해도 된다.
+
+        // 정리
+        // 정적 중첩 클래스는 사실 다른 클래스를 그저 중첩해 둔 것일 뿐이다.
+        // 쉽게 이야기 해서 둘은 아무런 관계가 없다.
+        // 정적 필드에 접근하는 것은 중첩 클래스가 아니어도 클래스명.정적 필드명을 통해 접근할 수 있기 때문이다.
+        // 따라서 클래스를 2개 따로 만든 것이라고 생각하면 된다.
+        // 유일한 차이점은 private에 접근할 수 있다는 것이다.
     }
 }
