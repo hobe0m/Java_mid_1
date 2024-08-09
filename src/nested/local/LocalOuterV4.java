3a41bb7e540e30676138c17389c76790dbfca801
hobe0m <beom231231@gmail.com>
2024-08-09 10:41:12 +0900

    [finish]Nested

--- src/nested/local/LocalOuterV4.java
+++ src/nested/local/LocalOuterV4.java
@@ -6,6 +6,12 @@ public class LocalOuterV4 {
     // 따라서 final로 선언하거나 또는 사실상 final이어야 한다.
     // 이것은 자바의 문법이고 규칙이다.
 
+    // 사실상 final
+    // 영어로는 effectively final이라고 한다.
+    // 사실상 final 지역 변수는 지역 변수에 final 키워드를 사용하지는 않았지만 값을 변경하지 않는 지역 변수를 뜻한다.
+    // final 키워드를 넣지 않았을 뿐이지, 실제로는 final 키워드를 넣은 것 처럼 중간에 값을 변경하지 않은 지역 변수이다.
+    // 따라서 사실상 final 지역 변수는 final 키워드를 넣어도 동일하게 작동해야 한다.
+
     private int outInstanceVar = 3;
 
     public Printer process(int paramVar) {
