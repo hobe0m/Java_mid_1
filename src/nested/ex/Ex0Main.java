4dcc3df95b5bf44483c665c7057910a63640bfcf
hobe0m <beom231231@gmail.com>
2024-08-12 17:30:38 +0900

    [finish]Nested

---
+++ src/nested/ex/Ex0Main.java
@@ -0,0 +1,22 @@
+package nested.ex;
+
+public class Ex0Main {
+    // 익명 클래스의 활용
+    // 활용 전 간단한 문제 풀이
+        public static void helloJava() {
+            System.out.println("프로그램 시작");
+            System.out.println("Hello Java");
+            System.out.println("프로그램 종료");
+        }
+
+        public static void helloSpring() {
+            System.out.println("프로그램 시작");
+            System.out.println("Hello Spring");
+            System.out.println("프로그램 종료");
+        }
+
+    public static void main(String[] args) {
+        helloJava();
+        helloSpring();
+    }
+}
