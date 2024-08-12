6aab5bdfe4c014a01a57cd95bfab9d647508041f
hobe0m <beom231231@gmail.com>
2024-08-12 16:57:48 +0900

    [finish]Nested

--- src/nested/anonymous/AnonymousOuter.java
+++ src/nested/anonymous/AnonymousOuter.java
@@ -1,5 +1,7 @@
 package nested.anonymous;
 
+import nested.local.Printer;
+
 public class AnonymousOuter {
     // 익명 클래스
     // 익명 클래스(Anonymous class)는 지역 클래스의 특별한 종류의 하나이다.
@@ -23,5 +25,30 @@ public class AnonymousOuter {
     // Printer printer = new Printer() {
     // }
 
+    private int outInstanceVar = 3;
 
+    public void process(int paramVar) {
+
+        int localVar = 1;
+
+        Printer printer = new Printer() {
+            int value = 0;
+
+            @Override
+            public void print() {
+                System.out.println("value = " + value);
+                System.out.println("localVar = " + localVar);
+                System.out.println("paramVar = " + paramVar);
+                System.out.println("outInstanceVar = " + outInstanceVar);
+            }
+        };
+
+        printer.print();
+        System.out.println("printer.class = " + printer.getClass());
+    }
+
+    public static void main(String[] args) {
+        AnonymousOuter main = new AnonymousOuter();
+        main.process(2);
+    }
 }
