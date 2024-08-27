9bf09398a6c5c3682272ef4fc2412e681618cec0
hobe0m <beom231231@gmail.com>
2024-08-27 09:25:35 +0900

    [finish]Exception

--- src/exception/ex2/NetworkServiceV2_4.java
+++ src/exception/ex2/NetworkServiceV2_4.java
@@ -15,5 +15,7 @@ public class NetworkServiceV2_4 {
 
             client.disconnect();
         }
+        // disconnect()를 호출해서 연결을 해제해야 했던 문제를 해결
+        // 외부 연결과 같은 자바 외부의 자원은 자동으로 해제가 되지 않으므로 외부 자원을 사용했다면 연결을 해제해서 외부 자원을 반납해야 한다.
     }
 }
