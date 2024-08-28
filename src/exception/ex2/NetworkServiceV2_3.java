f76fce3e802faca4f76590cff970ee33642a8a1f
hobe0m <beom231231@gmail.com>
2024-08-28 22:31:01 +0900

    [finish]Exception

--- src/exception/ex2/NetworkServiceV2_3.java
+++ src/exception/ex2/NetworkServiceV2_3.java
@@ -17,4 +17,7 @@ public class NetworkServiceV2_3 {
             System.out.println("[오류] 코드 : " + e.getErrorCode() + ", 메세지 : " + e.getMessage());
         }
     }
+
+    // 하나의 try 안에 정상 흐름을 모두 담고, 예외 부분은 catch 블럭에서 해결한다.
+    // 이렇게 하면 정상 흐름은 try 블럭에, 예외 흐름은 catch 블럭에 담겨 명확히 구분할 수 있다.
 }
