2741a5c45342e6ec282108065ee3ff89d9732b57
hobe0m <beom231231@gmail.com>
2024-08-29 21:12:46 +0900

    [finish]Exception

--- src/exception/ex2/MainV2.java
+++ src/exception/ex2/MainV2.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class MainV2 {
     public static void main(String[] args) throws NetworkClientExceptionV2 {
-        NetworkServiceV2_1 networkService = new NetworkServiceV2_1();
+        NetworkServiceV2_2 networkService = new NetworkServiceV2_2();
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
@@ -24,9 +24,19 @@ public class MainV2 {
     // 모든 곳에서 에러를 잡지 않으면, 결과적으로 main() 밖으로 예외가 던져지고, 그렇게 되면 프로그램이 종료된다.
     //  - 이 때, 예외 메세지와 예외를 추적할 수 있는 스택 트레이스가 같이 출력된다.
 
+    // V2_1
     // ServiceV2_1의 문제점
     // 예외 처리를 도입했지만 아직 예외가 복구되지는 않는다.
     //  - 즉, 예외가 발생하면 프로그램이 종료된다.
+
+    // 사용 후에 반드시 disconnect()를 통해 연결을 해제해야 한다.
+
+    // V2_2
+    // ServiceV2_2가 해결한 것
+    // 예외를 잡아서 처리했으므로 예외가 복구되고, 프로그램도 계속 수행할 수 있다.
+
+    // ServiceV2_2의 문제점
+    // 예외 처리를 했지만 정상 흐름과 예외 흐름이 섞여 있어서 코드를 읽기 어렵다.
 
     // 사용 후에 반드시 disconnect()를 통해 연결을 해제해야 한다.
 }
--- src/exception/ex2/NetworkServiceV2_2.java
+++ src/exception/ex2/NetworkServiceV2_2.java
@@ -8,8 +8,9 @@ public class NetworkServiceV2_2 {
         client.initError(data);
 
         try {
-            client.connect();;
+            client.connect();
         } catch (NetworkClientExceptionV2 e) {
+            // connect()에서 오류가 발생하면, 오류 코드와 오류 메세지를 출력하고 프로그램을 중지시킨다.
             System.out.println("[오류] 코드 : " + e.getErrorCode() + ", 메세지 : " + e.getMessage());
             return;
         }
@@ -21,8 +22,13 @@ public class NetworkServiceV2_2 {
         }
         client.disconnect();
     }
+    // connect(), send()같이 예외가 발생할 수 있는 곳에서 try ~ catch를 사용해서 NetworkClientExceptionV2 예외를 잡았다.
+    // 예외를 잡으면 오류 코드와 오류 메세지를 출력하게끔 설계했다.
+    // 여기서는 이렇게 예외를 잡아서 처리했기 때문에 이후에는 정상 흐름으로 복귀한다.
+    // 만약 예외를 잡았을 경우에는 return을 사용해 메서드를 빠져나간다.
+
     // 해결된 문제
-    // 예외를 잡아서 처리했기 때문에 예외가 복구되고 프로그램도 계속 수행할 수 있다.
+    // 예외를 잡아서 처리(try ~ catch)했기 때문에 예외가 복구되고 프로그램도 계속 수행할 수 있다.
 
     // 남은 문제
     // 예외 처리를 했지만 정상 흐름과 예외 흐름이 섞여 있어 코드를 읽기 어렵다.
